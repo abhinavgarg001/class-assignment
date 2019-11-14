d26aa49e7bbc8db9ab285e64163740a75ae4ac39
abhinavgarg001 <53693835+abhinavgarg001@users.noreply.github.com>
2019-11-14 11:40:28 +0530

    Add files via upload

---
+++ AufbauPrint.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node left,right;
+    Node(int data)
+    {
+        this.data=data;
+        left=null;
+        right=null;
+    }
+}
+static class insert
+{
+        
+}
+public class AufbauPrint
+{    
+	public static void main(String[] args)
+	{
+		Node new1=new Node();
+		
+	}
+}
---
+++ SubArrayCount.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SubArrayCount
+
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc=new Scanner(System.in);
+	   
+        System.out.println("Enter the size of array");
+
+	int n=sc.nextInt();
+
+	System.out.println("Enter value of k");
+	    
+        int k=sc.nextInt();
+	    
+        int m=n+1-k;
+	
+        int ans=(m*(m+1))/2;
+	    
+        System.out.println("No. of subarrays >= k = "+ans);
+	
+    }
+
+}
+
